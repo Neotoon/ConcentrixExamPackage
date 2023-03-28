63af11d307b59f7f85660d611b5b84baf54f13b8
Neotoon <61035850+Neotoon@users.noreply.github.com>
2023-03-28 23:13:36 +0800

    added button listeners

--- AngularProject/CRUD/src/app/employee-list/employee-list.component.ts
+++ AngularProject/CRUD/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CRUDService } from '../services/crud.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class EmployeeListComponent {
     { field: 'p_address', headerName:'Address', sortable:true,headerClass:'header-cell'},
     { field: 'p_email', headerName:'Email Address', sortable:true,headerClass:'header-cell'},
     { field: 'p_contact', headerName:'Contact Number', sortable:true,headerClass:'header-cell'},
-    { field: '', headerName:'Actions',width:250,headerClass:'header-cell',cellRenderer:this.actionRender},
+    { field: '', headerName:'Actions',width:250,headerClass:'header-cell',cellRenderer:this.actionRender.bind(this)},
   ];
 
   rowData:any = [];
@@ -24,7 +25,7 @@ export class EmployeeListComponent {
   employeeList:any=[]; 
   employeeListSubscribe:any;
 
-  constructor(private crudService:CRUDService){}
+  constructor(private crudService:CRUDService, private router:Router){}
 
   ngOnInit(): void{
     this.getEmployeeList();
@@ -43,6 +44,36 @@ export class EmployeeListComponent {
     '<button type="button" class="btn btn-danger">Delete</button>\n'+
     '<button type="button" class="btn btn-warning">Edit</button>'
     div.innerHTML=htmlCode;
+
+    //view button
+    let viewButton=div.querySelector('.btn-success');
+    viewButton?.addEventListener('click',()=>{
+      this.viewEmployeeDetails(params)
+    })
+
+     //edit button
+     let editButton=div.querySelector('.btn-warning');
+     editButton?.addEventListener('click',()=>{
+       this.editEmployeeDetails(params)
+     })
+
+     //delete button
+     let deleteButton=div.querySelector('.btn-danger');
+     deleteButton?.addEventListener('click',()=>{
+       this.deleteEmployee(params)
+     })
     return div;
+  }
+
+  viewEmployeeDetails(params: any){
+    this.router.navigate(['/crud/view-employee-details/'+params.data.p_id]);
+  }
+
+  editEmployeeDetails(params:any){
+    this.router.navigate(['/crud/update-employee/'+params.data.p_id]);
+  }
+
+  deleteEmployee(params:any){
+    console.log('latere')
   }
 }
