4662af380d4938be1b050aa547e2ccbdbfa0e710
Neotoon <61035850+Neotoon@users.noreply.github.com>
2023-03-28 17:14:18 +0800

    created employee component

--- FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./employees-list.component.css']
 })
 export class EmployeesListComponent {
+  employees: Employee[]=[
+    {
+      id:'1',
+      name:'John Doe',
+      email: 'john.doe@email.com',
+      phone: 998877665,
+      salary: 60000,
+      department: 'Human Resources'
+    },
+    {
+      id:'2',
+      name:'Sameer Saini',
+      email: 'sameer.saini@email.com',
+      phone: 789789789,
+      salary: 70000,
+      department: 'Information Technology'
+    },
+    {
+      id:'3',
+      name:'Samantha James',
+      email: 'samantha.james@email.com',
+      phone: 8787878787,
+      salary: 80000,
+      department: 'Accounting'
+    }
+  ]
 
+  ngOninit():void{
+  }
 }
