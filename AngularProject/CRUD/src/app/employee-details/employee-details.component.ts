ea131b3129d5cdac341dcba6e5703e166eec2b9d
Neotoon <61035850+Neotoon@users.noreply.github.com>
2023-03-29 12:35:13 +0800

    view employee details

--- AngularProject/CRUD/src/app/employee-details/employee-details.component.ts
+++ AngularProject/CRUD/src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from '../crud/models/employee';
 import { CRUDService } from '../services/crud.service';
 
 @Component({
@@ -7,7 +9,23 @@ import { CRUDService } from '../services/crud.service';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent {
+  //@ts-ignore
+  employeeDetails:Employee
 
-  constructor(private crudSerice:CRUDService){}
-
+  constructor(private crudSerice:CRUDService,
+    private activatedRoute:ActivatedRoute){}
+  ngOnInit(): void{
+    let employeeId='';
+    if(this.activatedRoute.snapshot.params['employeeId']){
+      employeeId=this.activatedRoute.snapshot.params['employeeId'];
+      if (employeeId!==''){
+        this.loadEmployeeDetails(employeeId)
+      }
+    }
+  }
+  loadEmployeeDetails(employeeId:any){
+    this.crudSerice.loadEmployeeDetails(employeeId).subscribe(res=>{
+    this.employeeDetails=res;
+      })
+  }
 }
