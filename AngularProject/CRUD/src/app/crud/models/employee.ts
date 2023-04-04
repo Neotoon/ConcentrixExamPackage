11a26f9756368e24879f590ca98a0a093e16113c
Neotoon <61035850+Neotoon@users.noreply.github.com>
2023-04-04 18:35:22 +0800

    Removed php version

--- AngularProject/CRUD/src/app/crud/models/employee.ts
+++
@@ -1,7 +0,0 @@
-export interface Employee {
-    p_id:any;
-    p_name:string;
-    p_address:string;
-    p_email:string;
-    p_contact:any;
-}
--- AngularProject/CRUD/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production:false,
-    API_EndPoint:'http://localhost/web_api/'
-};
